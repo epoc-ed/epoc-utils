417c819859026e1199662e3d13391bf67127958e
Erik Fröjdh <erik.frojdh@gmail.com>
2024-08-14 13:03:03 +0200

    removed rattler, starting with tests

--- examples/using_config.py
+++ examples/using_config.py
@@ -1,25 +1,15 @@
-import os
-from epoc import ConfigurationClient
+from epoc import ConfigurationClient, auth_token, redis_host
 
 
-#Get the password for the Redis server
-try:
-    token = os.environ['EPOC_REDIS_TOKEN']
-except KeyError:
-    raise ValueError('Please set the EPOC_REDIS_TOKEN environment variable')    
-    exit(1)
-    
-host = 'detpi02'
-
-cfg = ConfigurationClient(host, token=token)
+cfg = ConfigurationClient(redis_host(), token=auth_token())
 
 #Clear the database
-cfg.client.flushall()
+# cfg.client.flushall()
 
-cfg.PI_name = 'Erik'
-cfg.project_id = 'epoc'
-cfg.experiment_class = 'UniVie'
-cfg.base_data_dir = '/data/jungfrau/instruments/jem2100plus'
-cfg.measurement_tag = 'Lysozyme'
+# cfg.PI_name = 'Erik'
+# cfg.project_id = 'epoc'
+# cfg.experiment_class = 'UniVie'
+# cfg.base_data_dir = '/data/jungfrau/instruments/jem2100plus'
+# cfg.measurement_tag = 'Lysozyme'
 
 print(cfg)
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="epoc",
-    version= '2024.7.23',
+    version= '2024.8.14',
     author="Erik Frojdh",
     author_email="erik.frojdh@psi.ch",
     description="Configuration of experiments in the EPOC project",
